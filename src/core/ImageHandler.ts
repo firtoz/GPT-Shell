896ab75d6be8dfd438a944029caf6390ba9aa6c7
firtoz <hello@firtoz.com>
2023-01-05 14:16:53 +0000

    Adding the draw command.

--- src/core/ImageHandler.ts
+++ src/core/ImageHandler.ts
@@ -1,136 +1,247 @@
-import {AttachmentBuilder, Embed, EmbedBuilder, Message} from "discord.js";
-import {discordClient} from "../discord/discordClient";
+import {AttachmentBuilder, EmbedBuilder, Message} from "discord.js";
 import {CreateImageRequestSizeEnum, ImagesResponse, OpenAIApi} from "openai";
 import {v4} from "uuid";
 import {logMessage} from "../utils/logMessage";
-import {AxiosResponse} from "axios";
+import axios, {AxiosResponse} from "axios";
 import {db} from "../database/db";
+import {Stream} from "mongodb";
+import * as repl from "repl";
 
-class DiscordCachedMessage {
-    // private readonly embeds: (Embed | EmbedBuilder)[];
+type ImageHandlerRequestStorage = {
+    id: string;
+    finished: boolean;
+    description: string;
+    lastUpdated: number;
+    result: ImagesResponse | undefined;
+    channelId: string;
+    discordMessageId: string;
+};
 
-    constructor(private message: Message) {
-        // this.embeds = message.embeds;
+type ImageHandlerStorage = {
+    id: string;
+    userId: string;
+    lastUpdated: number;
+    size: CreateImageRequestSizeEnum;
+    subItems: ImageHandlerRequestStorage[];
+};
+
+
+class ImageHandlerRequest {
+    constructor(
+        public parent: ImageHandler,
+        public storage: ImageHandlerRequestStorage,
+    ) {
     }
 
-    static cache: Record<string, DiscordCachedMessage | undefined> = {};
+    async start(message: Message, isCommand: boolean = false) {
+        logMessage(`Starting ${this.storage.id}`, this.storage.description);
 
-    static async get(channelId: string, messageId: string) {
-        if (!DiscordCachedMessage.cache[messageId]) {
-            const channel = await discordClient.channels.fetch(channelId);
+        let replyPromise: Promise<Message>;
 
-            if (channel && channel.isTextBased()) {
-                const message = await channel.messages.fetch(messageId);
+        if (isCommand) {
+            replyPromise = message.edit({
+                embeds: [
+                    message.embeds[0],
+                    new EmbedBuilder()
+                        .setTitle('Image Prompt')
+                        .setDescription(`Generating:
+> ${this.storage.description}`),
+                ],
+            }).then(reply => {
+                this.storage.channelId = reply.channelId;
+                this.storage.discordMessageId = reply.id;
 
-                if (message) {
-                    DiscordCachedMessage.cache[messageId] = new DiscordCachedMessage(message);
-                }
-            }
+                return reply;
+            });
+        } else {
+            replyPromise = message.reply({
+                content: '',
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle('Image Prompt')
+                        .setDescription(`Generating:
+> ${this.storage.description}`),
+                ],
+            }).then(reply => {
+                this.storage.channelId = reply.channelId;
+                this.storage.discordMessageId = reply.id;
+
+                return reply;
+            });
         }
 
-        return DiscordCachedMessage.cache[messageId];
-    }
+        const responsePromise = this.parent.openai.createImage({
+            user: this.parent.storage.userId,
+            prompt: this.storage.description,
+            n: 1,
+            size: this.parent.storage.size,
+        });
 
-    private processorRunning = false;
-    private newItemAdded = false;
+        const response: AxiosResponse<ImagesResponse> = await responsePromise as any;
 
+        this.storage.result = response.data;
 
-    async setEmbed(embedIndex: number, embed: EmbedBuilder) {
-        this.embeds[embedIndex] = embed;
-        this.newItemAdded = true;
+        logMessage(`Response ${this.storage.id}`, this.storage.result)
 
-        if (!this.processorRunning) {
-            this.processorRunning = true;
-            while (this.newItemAdded) {
-                this.newItemAdded = false;
-                await this.message.edit({
-                    embeds: this.embeds,
-                });
-            }
-            this.processorRunning = false;
-        }
+        this.storage.finished = true;
+        this.storage.lastUpdated = new Date().getTime();
+
+        const axiosResponse: AxiosResponse<Stream> = await axios({
+            method: 'get',
+            url: response.data.data[0].url,
+            responseType: 'stream'
+        });
+
+        const attachment = new AttachmentBuilder(axiosResponse.data, {
+            name: `${this.storage.id}.png`,
+            description: this.storage.description,
+        });
+
+        const reply = await replyPromise;
+
+        const finalEmbed = new EmbedBuilder()
+            .setDescription(this.storage.description)
+            .setImage(`attachment://${this.storage.id}.png`);
+
+        await reply.edit({
+            files: [attachment],
+            embeds: isCommand ? [
+                message.embeds[0],
+                finalEmbed,
+            ] : [
+                finalEmbed,
+            ],
+        });
     }
 }
 
-export class ImageRequest {
-    discordMessageId: string;
-    channelId: string;
-    finished: boolean;
-    embedIndex: number;
-    lastUpdated: number;
-    public result: ImagesResponse | null = null;
+export class ImageHandler {
+    private requests: ImageHandlerRequest[];
 
-    constructor(public id: string, public userId: string, public description: string, public size: CreateImageRequestSizeEnum) {
-        this.channelId = '';
-        this.discordMessageId = '';
-        this.finished = false;
-        this.embedIndex = -1;
-        this.lastUpdated = new Date().getTime();
+    // private reply: Message | undefined = undefined;
+
+    constructor(public openai: OpenAIApi, public storage: ImageHandlerStorage) {
+        this.requests = storage.subItems.map(item => new ImageHandlerRequest(this, item));
     }
 
-    static async handle(openai: OpenAIApi, descriptions: string[], userId: string, messageToReplyTo: Message): Promise<void> {
-        let requests: ImageRequest[] = [];
+    static async handle(openai: OpenAIApi, descriptions: string[], userId: string, messageToReplyTo: Message, isCommand: boolean = false): Promise<void> {
+        const timeNow = new Date().getTime();
 
-        for (const description of descriptions) {
-            const request = new ImageRequest(v4(), userId, description.slice(0, 1000), '1024x1024');
-
-            requests.push(request);
-        }
-
-        const reply = await messageToReplyTo.reply({
-            content: '',
-            embeds: requests.map((request, index) => {
-                request.embedIndex = index;
-                return new EmbedBuilder()
-                    .setTitle('Image Prompt')
-                    .setDescription(`Generating:
-> ${request.description}`)
-            })
+        const request = new ImageHandler(openai, {
+            id: v4(),
+            userId,
+            size: '1024x1024',
+            subItems: descriptions.map((description): ImageHandlerRequestStorage => {
+                return {
+                    id: v4(),
+                    description,
+                    finished: false,
+                    lastUpdated: timeNow,
+                    result: undefined,
+                    channelId: '',
+                    discordMessageId: '',
+                }
+            }),
+            lastUpdated: timeNow,
         });
 
-        await Promise.all(requests.map(async item => {
-            item.discordMessageId = reply.id;
-            item.channelId = reply.channelId;
+        await request.handle(messageToReplyTo, isCommand);
+    }
 
-            await item.persist();
+    // async request(openai: OpenAIApi) {
+    //     const response: AxiosResponse<ImagesResponse> = await openai.createImage({
+    //         user: this.userId,
+    //         prompt: this.description.slice(0, 1000),
+    //         n: 1,
+    //         size: this.size,
+    //     }) as any;
+    //
+    //     this.result = response.data;
+    //     this.finished = true;
+    //
+    //     const message = await DiscordCachedMessage.get(this.channelId, this.discordMessageId);
+    //
+    //     if (message) {
+    //         await message.update(this);
+    //         // await message.setEmbed(
+    //         //     this.embedIndex,
+    //         //     new EmbedBuilder()
+    //         //         .setDescription(this.description)
+    //         //         .setImage(this.result!.data[0].url!),
+    //         // );
+    //     }
+    //
+    //     await this.persist();
+    // }
 
+    private persist() {
+        return db.set(`IMAGE-HANDLER-${this.storage.id}`, this.storage);
+    }
+
+    private async handle(messageToReplyTo: Message, isCommand: boolean = false) {
+        await this.persist();
+
+        await Promise.all(this.requests.map(async request => {
             try {
-                await item.request(openai);
+                await request.start(messageToReplyTo, isCommand);
             } catch (e) {
-                logMessage('Cannot do image request', item, e);
+                logMessage('Cannot do image request', request, e);
             }
         }));
     }
 
-    async request(openai: OpenAIApi) {
-        const response: AxiosResponse<ImagesResponse> = await openai.createImage({
-            user: this.userId,
-            prompt: this.description.slice(0, 1000),
-            n: 1,
-            size: this.size,
-        }) as any;
 
-        this.result = response.data;
-        this.finished = true;
+    // private processorRunning = false;
+    // private updateSignalled = false;
 
-        const message = await DiscordCachedMessage.get(this.channelId, this.discordMessageId);
-
-        if (message) {
-            await message.update(this);
-            // await message.setEmbed(
-            //     this.embedIndex,
-            //     new EmbedBuilder()
-            //         .setDescription(this.description)
-            //         .setImage(this.result!.data[0].url!),
-            // );
-        }
-
-        await this.persist();
-    }
-
-    private persist() {
-        return db.set(`IMAGE-REQUEST-${this.id}`, this);
-    }
+//     async signalUpdate() {
+//         this.updateSignalled = true;
+//
+//         if (!this.processorRunning) {
+//             this.processorRunning = true;
+//             while (this.updateSignalled) {
+//                 this.updateSignalled = false;
+//
+//                 try {
+//                     const reply = this.reply;
+//                     if (reply) {
+//                         const files = this.requests
+//                             .filter(item => item.attachment)
+//                             .map(item => {
+//                                 return item.attachment!;
+//                             })
+//
+//                         const embeds = this.requests.map(item => {
+//                             if (item.attachment) {
+//                                 return new EmbedBuilder()
+//                                     .setTitle('Image Prompt Finished')
+//                                     .setDescription(item.storage.description)
+//                                     .setImage(`attachment://${item.storage.id}.png`)
+//                             } else {
+//                                 return new EmbedBuilder()
+//                                     .setTitle('Image Prompt')
+//                                     .setDescription(`Generating...
+// > ${item.storage.description}`)
+//                             }
+//                         });
+//
+//                         await reply.edit({
+//                             content: '',
+//                             embeds,
+//                             files,
+//                         });
+//
+//                         await new Promise(resolve => setTimeout(resolve, 500));
+//                     }
+//
+//                     await this.persist();
+//                 } catch (e) {
+//                     logMessage(`Cannot update image handler ${this.storage.id}.`, e);
+//                 }
+//             }
+//             this.processorRunning = false;
+//         }
+//     }
 }
 
 export const extractDescriptions = (text: string): string[] => {
