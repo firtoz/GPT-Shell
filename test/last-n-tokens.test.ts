5a079d9dd416d698e52012eb32af3ef8214ee625
firtoz <hello@firtoz.com>
2023-01-11 11:11:32 +0000

    Fix test

--- test/last-n-tokens.test.ts
+++ test/last-n-tokens.test.ts
@@ -2,15 +2,45 @@ import {it, expect, describe} from 'vitest';
 
 import {getLastMessagesUntilMaxTokens} from "../src/core/GetLastMessagesUntilMaxTokens";
 
+const testMessage = 'hello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test message' +
+    'hello there this is a test messagehello there this is a test messagehello there this is a test messagehello' +
+    ' there this is a test messagehello there this is a test messagehello there this is a test messagehello there this' +
+    ' is a test messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is a test messagehello there this is a test messagehello there this is a test' +
+    ' messagehello there this is';
+
 describe('getLastMessagesUntilMaxTokens', () => {
     it('should return messages that sum up to less than the expected number', () => {
         const bcd = getLastMessagesUntilMaxTokens([
-            {content: 'a', numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 2400
-            {content: 'b', numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 1800
-            {content: 'c', numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 1200
-            {content: 'd', numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 600
+            {content: testMessage, numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 2400
+            {content: testMessage, numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 1800
+            {content: testMessage, numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 1200
+            {content: testMessage, numTokens: 600, id: '', embedding: null, type: 'human', timestamp: 0, userId: '', username: ''}, // 600
         ], 2000);
         expect(bcd).toHaveLength(3);
-        expect(bcd.map(item => item.content).join('')).to.eq('bcd');
+        expect(bcd.map(item => item.content).join('')).to.eq(testMessage + testMessage + testMessage);
     });
 });
